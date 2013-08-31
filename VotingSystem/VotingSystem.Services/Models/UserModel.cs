6f160cef1d6855a1b0eb765de71fce0419897b4a
Asen <alisp.programs@gmail.com>
2013-08-31 01:52:24 -0700

    Make small correction in UserModel

--- VotingSystem/VotingSystem.Services/Models/UserModel.cs
+++ VotingSystem/VotingSystem.Services/Models/UserModel.cs
@@ -6,43 +6,40 @@ using System.Web;
 
 namespace VotingSystem.Services.Models
 {
+    [DataContract]
+    public class UserLoginModel
+    {
+        [DataMember(Name = "username")]
+        public string Username { get; set; }
+
+        [DataMember(Name = "authCode")]
+        public string AuthCode { get; set; }
+    }
+
+    [DataContract]
+    public class UserRegisterModel : UserLoginModel
+    {
+        [DataMember(Name = "displayName")]
+        public string DisplayName { get; set; }
+    }
+
+    [DataContract]
     public class UserModel
     {
-        [DataContract]
-        public class UserLoginModel
-        {
-            [DataMember(Name = "username")]
-            public string Username { get; set; }
+        [DataMember(Name = "id")]
+        public int Id { get; set; }
 
-            [DataMember(Name = "authCode")]
-            public string AuthCode { get; set; }
-        }
+        [DataMember(Name = "displayName")]
+        public string DisplayName { get; set; }
+    }
 
-        [DataContract]
-        public class UserRegisterModel : UserLoginModel
-        {
-            [DataMember(Name = "displayName")]
-            public string DisplayName { get; set; }
-        }
+    [DataContract]
+    public class UserLoggedModel
+    {
+        [DataMember(Name = "sessionKey")]
+        public string SessionKey { get; set; }
 
-        [DataContract]
-        public class UserModel
-        {
-            [DataMember(Name = "id")]
-            public int Id { get; set; }
-
-            [DataMember(Name = "displayName")]
-            public string DisplayName { get; set; }
-        }
-
-        [DataContract]
-        public class UserLoggedModel
-        {
-            [DataMember(Name = "sessionKey")]
-            public string SessionKey { get; set; }
-
-            [DataMember(Name = "displayName")]
-            public string DisplayName { get; set; }
-        }
+        [DataMember(Name = "displayName")]
+        public string DisplayName { get; set; }
     }
 }
