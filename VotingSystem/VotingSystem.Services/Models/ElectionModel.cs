227794dd41f9151487d5f048c8709e5a866ea3ff
Velidar <you@example.com>
2013-09-02 17:59:23 +0300

    Added default values for properties in the Election Base model.

--- VotingSystem/VotingSystem.Services/Models/ElectionModel.cs
+++ VotingSystem/VotingSystem.Services/Models/ElectionModel.cs
@@ -46,8 +46,9 @@ namespace VotingSystem.Services.Models
             this.StateName = election.State.Name;
             this.StatusName = election.Status.Name;
             this.Owner = election.User.DisplayName;
+
             this.VotedUsersDisplayNamesString = "";
-            this.InvitedUsersDisplayNameString = "";
+            this.InvitedUsersDisplayNameString = ""; 
         }
 
         public ElectionBaseModel()
